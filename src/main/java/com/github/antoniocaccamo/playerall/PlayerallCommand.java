8b9c27102992671027766f497f5fd762227483fd
antonio caccamo <caccamo.antonio@gmail.com>
2019-11-07 18:03:59 +0100

    ADD : simple listener

--- src/main/java/com/github/antoniocaccamo/playerall/PlayerallCommand.java
+++ src/main/java/com/github/antoniocaccamo/playerall/PlayerallCommand.java
@@ -1,22 +1,29 @@
 package com.github.antoniocaccamo.playerall;
 
-import com.diffplug.common.swt.Coat;
+import com.diffplug.common.rx.RxBox;
+import com.diffplug.common.swt.Layouts;
 import com.diffplug.common.swt.Shells;
-
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.graphics.Point;
-
+import com.diffplug.common.swt.SwtMisc;
+import com.diffplug.common.swt.SwtRx;
+import com.diffplug.common.swt.jface.ViewerMisc;
 import io.micronaut.configuration.picocli.PicocliRunner;
-import io.micronaut.context.ApplicationContext;
-
-import picocli.CommandLine;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
 @Command(name = "playerall", description = "...",
         mixinStandardHelpOptions = true)
+
 public class PlayerallCommand implements Runnable {
+
+    private static Logger logger = LoggerFactory.getLogger(PlayerallCommand.class);
 
     @Option(names = {"-v", "--verbose"}, description = "...")
     boolean verbose;
@@ -32,7 +39,33 @@ public class PlayerallCommand implements Runnable {
         }
 
        Shells.builder(SWT.RESIZE | SWT.ICON | SWT.CLOSE, cmp -> {
+            Layouts.setGrid(cmp)
+                .numColumns(1)
+                .columnsEqualWidth(true)
+                .horizontalSpacing(0)
+                .verticalSpacing(0)
+            ;
 
+           Browser browser = new Browser(cmp, SWT.NONE);
+           Layouts.setGridData(browser)
+                   .grabHorizontal()
+                   .grabVertical()
+           ;
+           browser.setUrl("https://www.google.it");
+
+
+
+
+//           SwtRx.addListener(cmp,SWT.Move )
+//                   .subscribe(event ->  logger.info("event : {} cmp size : {}", event, cmp.getShell().getSize()));
+
+
+           SwtRx.addListener(cmp,SWT.Resize, SWT.Move )
+                   .subscribe(event ->  logger.info("event : {} | cmp size : {} location : {}", event, cmp.getSize(),  cmp.getLocation()));
+
+
+
+            
        })
         .setTitle("title")
         .setSize(400, 300)
